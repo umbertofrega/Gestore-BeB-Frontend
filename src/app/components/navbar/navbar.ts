5fcd45a90f235f7af15ad74c659e0ffe1d5ca0ab
umbertofrega <umbertofrega@gmail.com>
2025-12-03 16:30:16 +0100

    migrated navbar name

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import Keycloak from 'keycloak-js';
+import {AdminRole} from '../../models/enums/admin-role.model';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class Navbar implements OnInit {
 
+  public isLoggedIn = false;
+  public userRoles: string[] = [];
+
+  private readonly keycloak = inject(Keycloak);
+
+  async ngOnInit(): Promise<void> {
+    this.isLoggedIn = this.keycloak.authenticated ?? false;
+
+    if (this.isLoggedIn) {
+      this.userRoles = this.keycloak.realmAccess?.roles || [];
+    }
+  }
+
+  login() {
+    this.keycloak.login();
+  }
+
+  logout() {
+    this.keycloak.logout({ redirectUri: window.location.origin });
+  }
+
+  isAdminAreaVisible(): boolean {
+    return this.userRoles.includes(AdminRole.OWNER) || this.userRoles.includes(AdminRole.RECEPTIONIST);
+  }
 }
