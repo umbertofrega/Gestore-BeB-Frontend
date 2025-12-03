fba623561075a5f2041eb38ca4168c7017a5b659
umbertofrega <umbertofrega@gmail.com>
2025-12-03 13:26:24 +0100

    removed interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,12 @@
 import {provideRouter} from '@angular/router';
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideHttpClient} from '@angular/common/http';
 import {
-  createInterceptorCondition,
-  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-  IncludeBearerTokenCondition,
-  includeBearerTokenInterceptor,
   provideKeycloak
 } from 'keycloak-angular';
 
 import {routes} from './app.routes';
 
-const urlCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
-  urlPattern: /^(http:\/\/localhost:8080)(\/.*)?$/i,
-  bearerPrefix: 'Bearer'
-});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -29,12 +21,8 @@ export const appConfig: ApplicationConfig = {
         silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
       }
     }),
-    {
-      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-      useValue: [urlCondition]
-    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
+    provideHttpClient(),
   ]
 };
