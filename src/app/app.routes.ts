4fbcb19b08b667fbf9d0a176ad35bb111709b85f
umbertofrega <umbertofrega@gmail.com>
2025-12-03 13:26:09 +0100

    prepared routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import {Home} from './components/home/home';
+import {RoomDetail} from './components/room-detail/room-detail'; // Importa il componente!
 
 export const routes: Routes = [
-  // 5. 404 CATCH-ALL
-  { path: '**', redirectTo: '' },
+  // 1. Rotta Home (path vuoto)
+  { path: '', component: Home },
+
+  { path: 'room/:number', component : RoomDetail },
+
+  // 2. Rotta Catch-All (Wildcard) -> Va messa ALLA FINE
+  { path: '**', redirectTo: '' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {NavbarComponent} from './components/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
