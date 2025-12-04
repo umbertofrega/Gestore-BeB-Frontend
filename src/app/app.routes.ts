5bd2119fdc6c2a2565bb679c8447f6c6b217356a
umbertofrega <umbertofrega@gmail.com>
2025-12-04 23:37:40 +0100

    added route for profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import {Home} from './components/home/home';
 import {RoomDetail} from './components/room-detail/room-detail';
+import {Profile} from './components/profile/profile'
 import {AuthGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
 
   { path: 'room/:number', component : RoomDetail, canActivate: [AuthGuard] },
+
+  { path: 'me', component : Profile, canActivate: [AuthGuard] },
 
   { path: '**', redirectTo: '' }
 ];
