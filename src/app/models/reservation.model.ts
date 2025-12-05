366584240c539c8f260cff1af1fd5ebec77eaa98
umbertofrega <umbertofrega@gmail.com>
2025-12-05 23:07:20 +0100

    made id optional

--- src/app/models/reservation.model.ts
+++ src/app/models/reservation.model.ts
@@ -3,7 +3,7 @@ import {Guest} from './guest.model';
 import {PaymentStatus} from './enums/payment-status.model';
 
 export interface Reservation {
-  id: number;
+  id?: number;
   room: Room;
   guest: Guest;
   checkin: string;
