ab5c48234a8363edd72cc5a2312c6dc1c0596c10
umbertofrega <umbertofrega@gmail.com>
2025-12-05 23:10:32 +0100

    added reservation revoke

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,20 +1,25 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common'; // Import necessario per i template
+import {CommonModule} from '@angular/common';
 import {GuestService} from '../../services/guest.service';
 import {Guest} from '../../models/guest.model';
 import {Reservation} from '../../models/reservation.model';
+import {ReservationService} from '../../services/reservation.service';
+import {Router} from '@angular/router';
+import {differenceInDays} from 'date-fns';
 
 @Component({
   selector: 'app-profile',
-  standalone: true, // Ricorda standalone: true se usi inject() fuori dal costruttore
-  imports: [CommonModule], // Aggiungi CommonModule per il template
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile implements OnInit{
   guest : Guest | null = null;
   guestService = inject(GuestService)
+  reservationService = inject(ReservationService)
   reservations : Reservation[] = []
+  private router = inject(Router);
 
   ngOnInit(){
     this.guestService.getCurrentGuest().subscribe({
@@ -29,5 +34,34 @@ export class Profile implements OnInit{
       },
       error: (err) => console.error('Errore ricezione guest:', err)
     });
+  }
+
+  isRevocable(reservation : Reservation){
+    const checkinDate = new Date(reservation.checkin);
+
+    return differenceInDays(checkinDate, Date()) >= 7;
+  }
+
+  deleteReservation(reservationId: number | undefined){
+    if(!reservationId)
+      return;
+    if (!confirm("Sei sicuro di voler revocare questa prenotazione? L'azione è irreversibile.")) {
+      return;
+    }
+    this.reservationService.deleteReservation(reservationId).subscribe({
+      next: () => {
+        alert("Prenotazione eliminata con successo! Controlla la tua casella di posta elettronica per l'email di conferma.")
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error(`Errore nella revoca della prenotazione ${reservationId}:`, err);
+        alert(`Errore nella revoca: ${err.error.message}`);
+      }
+    });
+  }
+
+  protected hasPassed(checkout: string) {
+    const checkOutDate = new Date(checkout);
+    return differenceInDays(checkOutDate, Date()) < 0
   }
 }
