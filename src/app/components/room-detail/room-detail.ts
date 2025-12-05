47778d29d2a131bf676c569d90c92c90ce511831
umbertofrega <umbertofrega@gmail.com>
2025-12-05 23:06:51 +0100

    added reservation list

--- src/app/components/room-detail/room-detail.ts
+++ src/app/components/room-detail/room-detail.ts
@@ -1,7 +1,13 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {Room} from '../../models/room.model';
 import {RoomService} from '../../services/room.service';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Reservation} from '../../models/reservation.model';
+import {ReservationService} from '../../services/reservation.service';
+import {PaymentStatus} from '../../models/enums/payment-status.model';
+import {GuestService} from '../../services/guest.service';
+import {Guest} from '../../models/guest.model';
+import { differenceInDays } from 'date-fns';
 
 
 @Component({
@@ -14,6 +20,11 @@ export class RoomDetail implements OnInit{
 
   room : Room | null  = null;
   roomService : RoomService = inject(RoomService);
+  reservationService : ReservationService = inject(ReservationService);
+  guestService : GuestService = inject(GuestService);
+  reservation : Reservation | null = null;
+  guest : Guest | null = null;
+  private router = inject(Router);
 
   constructor(
     private route: ActivatedRoute,
@@ -22,19 +33,72 @@ export class RoomDetail implements OnInit{
 
   async ngOnInit() {
     const number = this.route.snapshot.paramMap.get('number');
-    if (number)
-      this.loadRoomDetail(number)
+
+    this.guestService.getCurrentGuest().subscribe({
+        next: (data) => {
+          this.guest = data;
+          if (number)
+            this.loadRoomDetail(number)
+        },
+      }
+    )
   }
 
   loadRoomDetail(roomNumber : String) {
     this.roomService.getRoom(+roomNumber).subscribe({
           next: (data) => {
             this.room = data;
-        },
+            this.loadReservation();
+          },
         error: (err) => console.error('Errore nella stanza passata:', err)
       },
     );
   }
 
+  loadReservation(){
+
+    if(this.guest == null) {
+      console.error("Errore nella registrazione della prenotazione, utente null")
+      return
+    }
+    if(this.room == null) {
+      console.error("Errore nella registrazione della prenotazione, stanza null")
+      return
+    }
+
+    const checkinString = this.route.snapshot.queryParams['checkin'];
+    const checkoutString = this.route.snapshot.queryParams['checkout'];
+
+    const checkinDate = new Date(checkinString);
+    const checkoutDate = new Date(checkoutString);
+
+    const totalNights = differenceInDays(checkinDate, checkoutDate)
+
+    const totalPrice = this.room.price * totalNights;
+
+     this.reservation = {
+      room: this.room,
+      guest: this.guest,
+      checkin: checkinString,
+      checkout: checkoutString,
+      price: totalPrice,
+      paymentStatus: PaymentStatus.PENDING,
+      createdAt: new Date().toISOString()
+    };
+  }
+
+  reserve(){
+    if (this.reservation) {
+      this.reservationService.addReservation(this.reservation).subscribe({
+        next: (res) => {
+          console.log('Prenotazione creata con successo:', res);
+          this.router.navigate(['/me']);
+        },
+        error: (err) => console.error('Errore nella creazione della prenotazione:', err)
+      });
+    } else {
+      console.error("Errore: Impossibile procedere, l'oggetto prenotazione è null.");
+    }
+  }
 
 }
