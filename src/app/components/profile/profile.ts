396a6cfb7f33ca36b4c72b2cef7c5b95521b39cd
umbertofrega <umbertofrega@gmail.com>
2025-12-05 01:12:31 +0100

    added profile page

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,26 +1,33 @@
 import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common'; // Import necessario per i template
 import {GuestService} from '../../services/guest.service';
 import {Guest} from '../../models/guest.model';
+import {Reservation} from '../../models/reservation.model';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true, // Ricorda standalone: true se usi inject() fuori dal costruttore
+  imports: [CommonModule], // Aggiungi CommonModule per il template
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile implements OnInit{
   guest : Guest | null = null;
   guestService = inject(GuestService)
-
+  reservations : Reservation[] = []
 
   ngOnInit(){
     this.guestService.getCurrentGuest().subscribe({
-      next: (data) => this.guest = data,
+      next: (guestData) => {
+        this.guest = guestData;
+        this.guestService.getReservations().subscribe({
+          next: (reservationsData) => {
+            this.reservations = reservationsData;
+          },
+          error: (err) => console.error('Errore ricezione prenotazioni:', err)
+        });
+      },
       error: (err) => console.error('Errore ricezione guest:', err)
-    })
-  }
-
-  getReservation(){
-
+    });
   }
 }
