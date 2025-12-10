d749afe70b40227b957b537600520ce41d93de59
umbertofrega <umbertofrega@gmail.com>
2025-12-10 18:32:35 +0100

    added checks on date input

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,27 +1,32 @@
 import {Component, OnInit, inject} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import Keycloak from 'keycloak-js';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {GuestService} from '../../services/guest.service';
 import {Guest} from '../../models/guest.model';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIcon} from '@angular/material/icon';
+import {setYear} from 'date-fns';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule, MatIconModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule, FormsModule, MatIcon, MatIcon],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar implements OnInit {
   guest : Guest | null = null;
   searchForm!: FormGroup;
+
   guestService = inject(GuestService)
   readonly keycloak = inject(Keycloak);
   private router = inject(Router);
   private fb = inject(FormBuilder);
+
+  today = new Date();
+  nextYear = new Date(setYear(this.today, this.today.getFullYear()+1));
 
   async ngOnInit(): Promise<void> {
     this.searchForm = this.fb.group({
@@ -31,7 +36,7 @@ export class Navbar implements OnInit {
       minGuests: [null]
     });
 
-    if(this.keycloak.authenticated)
+    if(this.keycloak.authenticated && !this.isAdmin())
       await this.syncUser()
   }
 
@@ -40,9 +45,46 @@ export class Navbar implements OnInit {
       alert("Inserisci le date per cercare!");
       return;
     }
+    const formValue = this.searchForm.value;
+    const dateA: string = formValue.checkin;
+    const dateB: string = formValue.checkout;
+
+    if (dateA === dateB) {
+      alert("Le date di arrivo e partenza devono essere diverse!");
+      return;
+    }
+
+    if(dateA < this.dateToString(this.today) || dateB < this.dateToString(this.today)) {
+      alert("Puoi prenotare solo da oggi in poi!")
+      return;
+    }
+
+    if(dateA > this.dateToString(this.nextYear) || dateB > this.dateToString(this.nextYear)) {
+      alert("Puoi prenotare per massimo l'anno prossimo!")
+      return;
+    }
+
+    let checkinDate : String;
+    let checkoutDate : String;
+
+    //Funziona perché la data è YYYY/MM/DD
+    if (dateA < dateB) {
+      checkinDate = dateA;
+      checkoutDate = dateB;
+    } else {
+      checkinDate = dateB;
+      checkoutDate = dateA;
+    }
+
+    const queryParams = {
+      checkin: checkinDate,
+      checkout: checkoutDate,
+      minSize: formValue.minSize,
+      minGuests: formValue.minGuests,
+    };
 
     this.router.navigate(['/'], {
-      queryParams: this.searchForm.value
+      queryParams: queryParams
     });
     }
 
@@ -93,4 +135,11 @@ export class Navbar implements OnInit {
     if(this.isAdmin())
       this.router.navigate(['/admin'])
   }
+
+  dateToString(data : Date | null) : string{
+    if (data)
+      return data.toISOString().split('T')[0]
+    return ''
+  }
+
 }
